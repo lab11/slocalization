29136aa1d6ad0530c1fdb475f54a2636963b914b
Ben Kempke <bpkempke@umich.edu>
2016-03-10 16:42:26 -0500

    positive results with out_manchester_works.bin

--- software/source/matlab/explore_backscatter.m
+++ software/source/matlab/explore_backscatter.m
@@ -4,6 +4,8 @@ gold_code = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1
 gold_len = length(gold_code);
 
 num_valid_cir_points = 1016;
+repetition_rate = 0.05; % Approximate number of seconds between beacon transmissions
+chip_rate = 1; % Modulation rate of slocalization tag
 data_segment_len = 8 + 1024*4 + 2 + 4 + 4;
 timestamp_to_one_second = 1*499.2e6*128;
 
@@ -14,7 +16,6 @@ num_data_segments = floor(file_size/data_segment_len);
 timestamps = zeros(num_data_segments,1);
 
 fid = fopen(file_name,'r');
-phase_offset = 0;
 
 % Get the first timestamp to start things off and know what to base everything off of
 overflow_cumsum = 0;
@@ -27,78 +28,111 @@ for ii=0:num_data_segments-1
 	end
 end
 
-% OPTIONAL: Cut off excess?
-gold_accum = zeros(gold_len,num_valid_cir_points);
-gold_accum_num = zeros(gold_len,1);
-gold_bits = zeros(num_data_segments,1);
-rxpaccs = zeros(num_data_segments,1);
-cir_pwrs = zeros(num_data_segments,1);
-round_nums = zeros(num_data_segments,1);
-fp_idxs = zeros(num_data_segments,1);
-cirs = zeros(1000,num_valid_cir_points);
-rxpaccs_mod = zeros(1000,1);
-mags = zeros(num_data_segments,1);
-for ii=0:num_data_segments-1
-	%if ii < 1e4 || ii > 2.3e4
-	%if ii < 2e4
-	%	continue;
-	%end
-	fseek(fid,ii*data_segment_len+8,'bof');
-	cur_cir_data = fread(fid,num_valid_cir_points*2,'int16');
-	cur_cir_data = cur_cir_data(1:2:end) + 1i*cur_cir_data(2:2:end);
-	fseek(fid,(ii+1)*data_segment_len-10,'bof');
-	round_nums(ii+1) = fread(fid,1,'uint32');
-	fp_idx = fread(fid,1,'uint16');
-	%if fp_idx < 4.803e4 || fp_idx > 4.806e4
-	%	continue;
-	%end
-	fp_idxs(ii+1) = fp_idx;
-	finfo = fread(fid,1,'uint32');
-	%rxpaccs(ii+1) = bitand(finfo,hex2dec('FFF00000'))/2^20;
-	cir_pwrs(ii+1) = bitand(finfo,hex2dec('0000FFFF'));
+gold_corrs_full = zeros(4, gold_len, num_valid_cir_points);
+median_bit_periods = zeros(1000,num_valid_cir_points);
+median_bit_periods_num = 0;
+blah = zeros(num_data_segments,1);
+for time_idx = 0:3
+	% OPTIONAL: Cut off excess?
+	gold_accum = zeros(gold_len,num_valid_cir_points);
+	gold_accum_num = zeros(gold_len,1);
+	gold_bits = zeros(num_data_segments,1);
+	rxpaccs = zeros(num_data_segments,1);
+	cir_pwrs = zeros(num_data_segments,1);
+	round_nums = zeros(num_data_segments,1);
+	fp_idxs = zeros(num_data_segments,1);
+	cirs = zeros(num_data_segments,num_valid_cir_points);
+	rxpaccs_mod = zeros(1000,1);
+	bit_period_median_filter = zeros(ceil(chip_rate/repetition_rate), num_valid_cir_points);
+	bit_period_chips = zeros(ceil(chip_rate/repetition_rate),1);
+	bit_period_num = 0;
+	last_bit_number = 1;
+	time_offset = time_idx * 0.5;
+	for ii=0:num_data_segments-1
+		fseek(fid,ii*data_segment_len+8,'bof');
+		cur_cir_data = fread(fid,num_valid_cir_points*2,'int16');
+		cur_cir_data = cur_cir_data(1:2:end) + 1i*cur_cir_data(2:2:end);
+		fseek(fid,(ii+1)*data_segment_len-10,'bof');
+		round_nums(ii+1) = fread(fid,1,'uint32');
+		fp_idx = fread(fid,1,'uint16');
+		fp_idxs(ii+1) = fp_idx;
+		%if fp_idx > 4.7665e4 || fp_idx < 4.7635e4
+		%	continue;
+		%end
+		finfo = fread(fid,1,'uint32');
+		rxpaccs(ii+1) = bitand(finfo,hex2dec('FFF00000'))/2^20;
+		%cir_pwrs(ii+1) = bitand(finfo,hex2dec('0000FFFF'));
+	
+		%%UNSYNCHRONIZED: Rotate CIRs back to t = 0
+		%cur_cir_data_fft = fft(cur_cir_data);
+		%cur_cir_data_fft = cur_cir_data_fft.*exp(1i*2*pi*fftshift(0:num_valid_cir_points-1).*fp_idx./64./num_valid_cir_points).';
+		%cur_cir_data = ifft(cur_cir_data_fft);
+	
+		%Normalize by the magnitude of leading edge
+		rxpaccs(ii+1) = abs(cur_cir_data(750));%sum(abs(cur_cir_data(754:764)));%
+		%if(rxpaccs(ii+1) < 1e4)
+		%	continue;
+		%end
+		cur_cir_data = cur_cir_data.*exp(-1i*angle(cur_cir_data(750)));
+		
+	
+		%cur_cir_data = cur_cir_data./rxpaccs(ii+1);
+	
+		cirs(ii+1,:) = cur_cir_data;
+		rxpaccs_mod(mod(ii,1000)+1) = rxpaccs(ii+1);
+	
+		chip_number = floor((timestamps(ii+1) - timestamps(1))/timestamp_to_one_second + time_offset) + 1;
+		bit_number = floor((chip_number-1)/2) + 1;
+		blah(ii+1) = cur_cir_data(757);
 
-	%%UNSYNCHRONIZED: Rotate CIRs back to t = 0
-	%cur_cir_data_fft = fft(cur_cir_data);
-	%cur_cir_data_fft = cur_cir_data_fft.*exp(1i*2*pi*fftshift(0:num_valid_cir_points-1).*fp_idx./64./num_valid_cir_points).';
-	%cur_cir_data = ifft(cur_cir_data_fft);
-
-	rxpaccs(ii+1) = abs(cur_cir_data(752));%sum(abs(cur_cir_data(754:764)));
-	if(rxpaccs(ii+1) < 1e4)
-		continue;
+		% Check to see if we've transitioned a bit, in which case we need to go back, median filter, and extract manchester-encoded 
+		% bit CIRs
+		if bit_number > last_bit_number
+			bit_number_mod = mod(bit_number-1,gold_len)+1;
+			last_bit_number = bit_number;
+			median_bit_period = median(bit_period_median_filter(1:bit_period_num,:),1);
+			zero_chip_idxs = find(bit_period_chips(1:bit_period_num) == 0);
+			one_chip_idxs = find(bit_period_chips(1:bit_period_num) == 1);
+			median_zero_chips = median(bit_period_median_filter(zero_chip_idxs,:),1) - median_bit_period;
+			median_one_chips = median(bit_period_median_filter(one_chip_idxs,:),1) - median_bit_period;
+			bit_period_num = 0;
+			if any(isnan([median_zero_chips,median_one_chips]))
+				continue;
+			end
+			gold_accum(bit_number_mod,:) = gold_accum(bit_number_mod,:) + median_zero_chips - median_one_chips;
+			gold_accum_num(bit_number_mod) = gold_accum_num(bit_number_mod) + 1;
+			median_bit_periods_num = median_bit_periods_num + 1;
+			median_bit_periods(median_bit_periods_num,:) = median_bit_period;
+		end
+		bit_period_num = bit_period_num + 1;
+		bit_period_median_filter(bit_period_num,:) = cur_cir_data;
+		bit_period_chips(bit_period_num) = mod(chip_number-1,2);
 	end
-	cur_cir_data = cur_cir_data.*exp(-1i*angle(cur_cir_data(752)));
 	
-
-	cur_cir_data = cur_cir_data./rxpaccs(ii+1);
-	mags(ii+1) = cur_cir_data(757);
-
-	cirs(mod(ii,1000)+1,:) = cur_cir_data;
-	rxpaccs_mod(mod(ii,1000)+1) = rxpaccs(ii+1);
-
-	time_offset = (timestamps(ii+1) - timestamps(1))/timestamp_to_one_second + 0.5;
-	cur_gold_bit = mod(floor(time_offset),gold_len)+1;
-	gold_accum(cur_gold_bit,:) = gold_accum(cur_gold_bit,:) + cur_cir_data.';
-	gold_accum_num(cur_gold_bit) = gold_accum_num(cur_gold_bit) + 1;
-	gold_bits(ii+1) = cur_gold_bit;
+	%Normalize to the number of accumulated CIRs in each set
+	gold_accum = gold_accum./repmat(gold_accum_num,[1,num_valid_cir_points]);
+	%gold_accum = gold_accum-repmat(median(gold_accum,1),[gold_len,1]);
+	%gold_accum = abs(gold_accum);
+	%gold_accum = gold_accum-repmat(median(gold_accum),[gold_len,1]);
+	
+	%Perform cross correlation across accumulated CIRs
+	gold_corrs = zeros(gold_len, num_valid_cir_points);
+	for ii=0:gold_len-1
+		gold_rotated = circshift(gold_code.',ii);
+		pn_ones = find(gold_rotated == 1);
+		pn_zeros = find(gold_rotated == -1);
+		pn_ones_mean = sum(gold_accum(pn_ones,:),1);
+		pn_zeros_mean = sum(gold_accum(pn_zeros,:),1);
+		
+		gold_corrs(ii+1,:) = pn_ones_mean - pn_zeros_mean;%sum(gold_accum.*repmat(gold_rotated,[1,num_valid_cir_points]),1);
+	end
+	gold_corrs_full(time_idx+1,:,:) = gold_corrs;
+end
+for ii=1:4
+	subplot(2,2,ii);
+	imagesc(squeeze(abs(gold_corrs_full(ii,:,750:800))))
 end
 
-%Normalize to the number of accumulated CIRs in each set
-gold_accum = gold_accum./repmat(gold_accum_num,[1,num_valid_cir_points]);
-gold_accum = gold_accum-repmat(median(gold_accum,1),[gold_len,1]);
-%gold_accum = abs(gold_accum);
-%gold_accum = gold_accum-repmat(median(gold_accum),[gold_len,1]);
-
-%Perform cross correlation across accumulated CIRs
-gold_corrs = zeros(gold_len, num_valid_cir_points);
-for ii=0:gold_len-1
-	gold_rotated = circshift(gold_code.',ii);
-	pn_ones = find(gold_rotated == 1);
-	pn_zeros = find(gold_rotated == -1);
-	pn_ones_mean = sum(gold_accum(pn_ones,:),1);
-	pn_zeros_mean = sum(gold_accum(pn_zeros,:),1);
-	
-	gold_corrs(ii+1,:) = pn_ones_mean - pn_zeros_mean;%sum(gold_accum.*repmat(gold_rotated,[1,num_valid_cir_points]),1);
-end
 return;
 
 INTERP_SIZE = 2^16;
