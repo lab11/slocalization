3cd2988a8b74125b16967e40dba552cf4f5fb9c4
Ben Kempke <bpkempke@umich.edu>
2017-01-03 20:09:49 -0500

    barebones code will continuously send and receive at START_FREQ

--- gr-slocalization/python/uwb_bandstitching.py
+++ gr-slocalization/python/uwb_bandstitching.py
@@ -31,11 +31,22 @@ from gnuradio import eng_notation
 
 n2s = eng_notation.num_to_str
 
-SAMPLE_RATE = 1000e6
+SAMPLE_RATE = 4e6
+START_FREQ = 3.1e9
+END_FREQ = 4.4e9
+STEP_FREQ = 4e6
 
 class build_block(gr.top_block):
     def __init__(self, args1, args2):
         gr.top_block.__init__(self)
+
+        ##############################
+        # TRANSMIT CHAIN
+        ##############################
+        print "\nTRANSMIT CHAIN"
+
+        #USRP transmits repeating file generated in MATLAB
+        self.tx_src = blocks.file_source(gr.sizeof_gr_complex, "iq_in.dat", True)
 
         #Find USRP with device characteristics specified by args1
         d1 = uhd.find_devices(uhd.device_addr(args1))
@@ -43,72 +54,43 @@ class build_block(gr.top_block):
         print "\nFound '%s' at args '%s'" % \
             (uhd_type1, args1)
 
+        stream_args = uhd.stream_args('fc32')
+        self.u_tx = uhd.usrp_sink(device_addr=args1, stream_args=stream_args)
+        self.u_tx.set_samp_rate(SAMPLE_RATE)
+        self.u_tx.set_center_freq(START_FREQ)
+
+        # Get dboard gain range and select maximum
+        tx_gain_range = self.u_tx.get_gain_range()
+        tx_gain = tx_gain_range.stop()
+        self.u_tx.set_gain(tx_gain)
+
+        self.connect (self.tx_src, self.u_tx)
+
+        ##############################
+        # RECEIVE CHAIN
+        ##############################
+        print "\nRECEIVE CHAIN"
+
+        #USRP logs IQ data to file
+        self.rx_dst = blocks.file_meta_sink(gr.sizeof_gr_complex, "iq_out.dat", SAMPLE_RATE)
+
         #Find USRP with device characteristics specified by args1
         d2 = uhd.find_devices(uhd.device_addr(args2))
         uhd_type2 = d2[0].get('type')
         print "\nFound '%s' at args '%s'" % \
             (uhd_type2, args2)
 
-        print "\nTRANSMIT CHAIN"
-        stream_args = uhd.stream_args('fc32')
-        self.u_tx = uhd.usrp_sink(device_addr=args1, stream_args=stream_args)
-        self.u_tx.set_samp_rate(SAMPLE_RATE)
-
-        self.tx_src0 = analog.sig_source_c(self.u_tx.get_samp_rate(),
-                                           analog.GR_CONST_WAVE,
-                                           0, 1.0, 0)
-
-        # Get dboard gain range and select maximum
-        tx_gain_range = self.u_tx.get_gain_range()
-        tx_gain = tx_gain_range.stop()
-
-        # Get dboard freq range and select midpoint
-        tx_freq_range = self.u_tx.get_freq_range()
-        tx_freq_mid = (tx_freq_range.start() + tx_freq_range.stop())/2.0
-
-        for i in xrange(tx_nchan):
-            self.u_tx.set_center_freq (tx_freq_mid + i*1e6, i)
-            self.u_tx.set_gain(tx_gain, i)
-
-        print "\nTx Sample Rate: %ssps" % (n2s(self.u_tx.get_samp_rate()))
-        for i in xrange(tx_nchan):
-            print "Tx Channel %d: " % (i)
-            print "\tFrequency = %sHz" % \
-                (n2s(self.u_tx.get_center_freq(i)))
-            print "\tGain = %f dB" % (self.u_tx.get_gain(i))
-        print ""
-
-        self.connect (self.tx_src0, self.u_tx)
-
-        print "\nRECEIVE CHAIN"
         self.u_rx = uhd.usrp_source(device_addr=args2,
-                                    io_type=uhd.io_type.COMPLEX_FLOAT32,
-                                    num_channels=rx_nchan)
-        self.rx_dst0 = blocks.null_sink(gr.sizeof_gr_complex)
-
+                                    io_type=uhd.io_type.COMPLEX_FLOAT32)
         self.u_rx.set_samp_rate(SAMPLE_RATE)
+        self.u_rx.set_center_freq(START_FREQ)
 
         # Get dboard gain range and select maximum
         rx_gain_range = self.u_rx.get_gain_range()
         rx_gain = rx_gain_range.stop()
+        self.u_rx.set_gain(rx_gain, i)
 
-        # Get dboard freq range and select midpoint
-        rx_freq_range = self.u_rx.get_freq_range()
-        rx_freq_mid = (rx_freq_range.start() + rx_freq_range.stop())/2.0
-
-        for i in xrange(tx_nchan):
-            self.u_rx.set_center_freq (rx_freq_mid + i*1e6, i)
-            self.u_rx.set_gain(rx_gain, i)
-
-        print "\nRx Sample Rate: %ssps" % (n2s(self.u_rx.get_samp_rate()))
-        for i in xrange(rx_nchan):
-            print "Rx Channel %d: " % (i)
-            print "\tFrequency = %sHz" % \
-                (n2s(self.u_rx.get_center_freq(i)))
-            print "\tGain = %f dB" % (self.u_rx.get_gain(i))
-        print ""
-
-        self.connect (self.u_rx, self.rx_dst0)
+        self.connect (self.u_rx, self.rx_dst)
 
 def main ():
     parser = OptionParser (option_class=eng_option)
