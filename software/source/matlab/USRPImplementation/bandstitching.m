9be20b31541b394f5cbd85bd78685561c0db549b
Ben Kempke <bpkempke@umich.edu>
2017-02-13 01:17:20 -0500

    best to just do tag frequency analysis offline for now

--- software/source/matlab/USRPImplementation/bandstitching.m
+++ software/source/matlab/USRPImplementation/bandstitching.m
@@ -27,38 +27,40 @@ overair_data_times = overair_data_times(VALID_MEAS_START_IDX:end,:,:);
 
 %In order to locate the backscatter tag, we need to search across <tag_freq,quadrature_phase>
 tag_freq_search_space = -TAG_FREQ_PPM_ACCURACY:1e-4:TAG_FREQ_PPM_ACCURACY;
-tag_search_corr = zeros(length(tag_freq_search_space),size(overair_data,4));
+%tag_search_corr = zeros(length(tag_freq_search_space),size(overair_data,4));
+%for tag_freq_offset_idx = 1:length(tag_freq_search_space)
+%    tag_freq_offset_ppm = tag_freq_search_space(tag_freq_offset_idx);
+%    cur_search_tag_freq = 1/TAG_FREQ*(1+tag_freq_offset_ppm);
+%    %for quadrature_phase = 1:2
+%        %overair_data_times_mod = rem(overair_data_times + quadrature_phase/TAG_FREQ/4, cur_search_tag_freq);
+%        %overair_data_zero_idxs = overair_data_times_mod < cur_search_tag_freq/2;
+%        %overair_data_one_idxs = overair_data_times_mod >= cur_search_tag_freq/2;
+%        %overair_data_temp = overair_data;
+%        %overair_data_temp(:,overair_data_zero_idxs) = -overair_data_temp(:,overair_data_zero_idxs);
+%        %tag_search_corr(tag_freq_offset_idx,quadrature_phase,:) = shiftdim(sum(sum(abs(sum(overair_data_temp,2)),3),1),1);
+%    %end
+%    mixing_signal = exp(-1i.*2.*pi./cur_search_tag_freq.*overair_data_times(:,1,1));
+%    overair_data_temp = overair_data.*repmat((blackman(length(mixing_signal)).*mixing_signal).',[size(overair_data,1),1,size(overair_data,3),size(overair_data,4)]);
+%    tag_search_corr(tag_freq_offset_idx,:) = sum(sum(abs(sum(overair_data_temp,2)),3),1);
+%    disp(['computed tag_freq_offset_ppm = ', num2str(tag_freq_offset_ppm)])
+%end
+%
+%%Best correlation is used
+%[~,best_freq_offs_idxs] = max(tag_search_corr,[],1);
+%tag_freq_offset = tag_freq_search_space(best_freq_offs_idxs);
+
+deconvolved = zeros(size(overair_data,1)/2*size(overair_data,3),size(overair_data,4),length(tag_freq_search_space));
 for tag_freq_offset_idx = 1:length(tag_freq_search_space)
-    tag_freq_offset_ppm = tag_freq_search_space(tag_freq_offset_idx);
-    cur_search_tag_freq = 1/TAG_FREQ*(1+tag_freq_offset_ppm);
-    %for quadrature_phase = 1:2
-        %overair_data_times_mod = rem(overair_data_times + quadrature_phase/TAG_FREQ/4, cur_search_tag_freq);
-        %overair_data_zero_idxs = overair_data_times_mod < cur_search_tag_freq/2;
-        %overair_data_one_idxs = overair_data_times_mod >= cur_search_tag_freq/2;
-        %overair_data_temp = overair_data;
-        %overair_data_temp(:,overair_data_zero_idxs) = -overair_data_temp(:,overair_data_zero_idxs);
-        %tag_search_corr(tag_freq_offset_idx,quadrature_phase,:) = shiftdim(sum(sum(abs(sum(overair_data_temp,2)),3),1),1);
-    %end
-    mixing_signal = exp(-1i.*2.*pi./cur_search_tag_freq.*overair_data_times(:,1,1));
-    overair_data_temp = overair_data.*repmat((blackman(length(mixing_signal)).*mixing_signal).',[size(overair_data,1),1,size(overair_data,3),size(overair_data,4)]);
-    tag_search_corr(tag_freq_offset_idx,:) = sum(sum(abs(sum(overair_data_temp,2)),3),1);
-    disp(['computed tag_freq_offset_ppm = ', num2str(tag_freq_offset_ppm)])
-end
-
-%Best correlation is used
-[~,best_freq_offs_idxs] = max(tag_search_corr,[],1);
-tag_freq_offset = tag_freq_search_space(best_freq_offs_idxs);
-
-tag_freq = 1./TAG_FREQ*(1+tag_freq_offset);
-for quadrature_phase = 1:2
-    %mixing_signal = sign(cos(2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times + quadrature_phase*pi/2));
-    %mixing_signal = exp(1i.*2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times + quadrature_phase*pi/2);
-    %mixing_signal = cos(2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times + quadrature_phase*pi/2);
+    tag_freq = 1./TAG_FREQ*(1+tag_freq_search_space(tag_freq_offset_idx));
+    disp(['computing tag_freq=',num2str(tag_freq)])
+    %mixing_signal = sign(cos(2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times));
+    %mixing_signal = exp(1i.*2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times);
+    %mixing_signal = cos(2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times);
     mixing_signal = ones(size(overair_data_times));
-    mixing_signal = mixing_signal.*repmat(blackman(size(mixing_signal,1)),[1,size(mixing_signal,2),size(mixing_signal,3)]);
+    %mixing_signal = mixing_signal.*repmat(blackman(size(mixing_signal,1)),[1,size(mixing_signal,2),size(mixing_signal,3)]);
     overair_data_temp = overair_data.*repmat(shiftdim(mixing_signal,-1),[size(overair_data,1),1,1,1]);
     %overair_data_temp = overair_data;
-    keyboard;
+    %keyboard;
 
     %Average across time...
     overair_data_temp = squeeze(sum(overair_data_temp,2));
@@ -78,17 +80,16 @@ for quadrature_phase = 1:2
     %For now, DC and -BW/2 are unusable, so only choose every other bin for now...
     deconvolved_fft = deconvolved_fft(2:2:end,:,:);
 
-    keyboard;
+    %keyboard;
     
     %Flatten to one dimension
     deconvolved_fft = reshape(deconvolved_fft,[size(deconvolved_fft,1)*size(deconvolved_fft,2),size(deconvolved_fft,3)]);
     
     %Load and apply window function
-    deconvolved_fft = deconvolved_fft.*repmat(blackman(size(deconvolved_fft,1)),[1,size(deconvolved_fft,2)]);
+    deconvolved_fft = deconvolved_fft.*repmat(hamming(size(deconvolved_fft,1)),[1,size(deconvolved_fft,2)]);
     deconvolved_fft = ifftshift(deconvolved_fft,1);
     
     %Inverse FFT = CIR
-    deconvolved = ifft(deconvolved_fft,[],1);
+    deconvolved(:,:,tag_freq_offset_idx) = ifft(deconvolved_fft,[],1);
 
-    keyboard;
 end
