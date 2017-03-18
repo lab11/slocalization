7addb3047b1018297c64daf34fedaceb5b42df10
Ben Kempke <bpkempke@umich.edu>
2017-03-17 21:13:52 -0400

    working on bandstitching and tag CIR extraction

--- software/source/matlab/USRPImplementation/bandstitching.m
+++ software/source/matlab/USRPImplementation/bandstitching.m
@@ -1,13 +1,13 @@
-function deconvolved = bandstitching(in_header_filename, in_data_filename)
+function deconvolved = bandstitching(in_header_filename, in_data_filename, in_cal_header_filename, in_cal_data_filename)
 
 VALID_MEAS_START_IDX = 100;
 CAL_VALID_MEAS_START_IDX = 100;
 SAMPLE_RATE = 25e6;
 ACCUM_COUNT = 1e3;
 TAG_FREQ = 256;
-TAG_FREQ_PPM_ACCURACY = 250e-5;
+TAG_FREQ_PPM_ACCURACY = 50e-5;
 
-[cal_data, cal_data_times] = readUSRPData('header_direct3.csv','data_direct3.dat', SAMPLE_RATE, ACCUM_COUNT);
+[cal_data, cal_data_times] = readUSRPData(in_cal_header_filename,in_cal_data_filename, SAMPLE_RATE, ACCUM_COUNT);
 cal_data = cal_data(:,CAL_VALID_MEAS_START_IDX:end,:,:);
 
 %For now, let's just use the last bandstitching sweep...
@@ -26,7 +26,7 @@ overair_data_times = overair_data_times(VALID_MEAS_START_IDX:end,:,:);
 %overair_data_times = overair_data_times(1:end-100+best_ending_idx-1,:,:);
 
 %In order to locate the backscatter tag, we need to search across <tag_freq,quadrature_phase>
-tag_freq_search_space = -TAG_FREQ_PPM_ACCURACY:1e-4:TAG_FREQ_PPM_ACCURACY;
+tag_freq_search_space = -TAG_FREQ_PPM_ACCURACY:5e-6:TAG_FREQ_PPM_ACCURACY;
 %tag_search_corr = zeros(length(tag_freq_search_space),size(overair_data,4));
 %for tag_freq_offset_idx = 1:length(tag_freq_search_space)
 %    tag_freq_offset_ppm = tag_freq_search_space(tag_freq_offset_idx);
@@ -49,6 +49,35 @@ tag_freq_search_space = -TAG_FREQ_PPM_ACCURACY:1e-4:TAG_FREQ_PPM_ACCURACY;
 %[~,best_freq_offs_idxs] = max(tag_search_corr,[],1);
 %tag_freq_offset = tag_freq_search_space(best_freq_offs_idxs);
 
+%%Deconvolution with over-air direct (doesn't work...)
+%cal_data = sum(overair_data(:,:,:,2),2);
+%cal_data_fft = squeeze(fft(cal_data,[],1));
+
+%Due to 25 MHz steps and 100 MHz reference, we observe a random pi/2 slip each retuning
+phase_correction = zeros(size(overair_data,3),size(overair_data,4));
+
+%Phase slip of first is our reference and set to zero
+cand_phase_corrs = (0:7)*pi/4;
+for ii=2:size(overair_data,3)
+    for jj=1:size(overair_data,4)
+        prev_step_ts = sum(overair_data(:,:,ii-1,jj),2).*exp(1i*phase_correction(ii-1,jj));
+        prev_step_fft = fftshift(fft(prev_step_ts)./cal_data_fft(:,ii-1));
+        cur_step_ts = sum(overair_data(:,:,ii,jj),2);
+        cur_step_fft = fftshift(fft(cur_step_ts)./cal_data_fft(:,ii));
+
+        %Try all phase offsets and pick the one with lowest quadrant magnitude
+        cand_bandstitched_combinations_fft = repmat([prev_step_fft(2:2:end);cur_step_fft(2:2:end)].*hamming(length(cur_step_fft)),[1,length(cand_phase_corrs)]).*[ones(length(prev_step_fft)/2,length(cand_phase_corrs));repmat(exp(1i*cand_phase_corrs),[length(cur_step_fft)/2,1])];
+        cand_bandstitched_combinations_ts = ifft(ifftshift(cand_bandstitched_combinations_fft,1),[],1);
+        num_bins_per_quadrant = floor(size(cand_bandstitched_combinations_ts,1)/4);
+        quadrants_ts = reshape(abs(cand_bandstitched_combinations_ts(1:num_bins_per_quadrant*4,:)),[num_bins_per_quadrant,4,size(cand_bandstitched_combinations_ts,2)]);
+        min_quadrants = min(squeeze(sum(quadrants_ts,1)),[],1);
+        [~,which_corr] = min(sum(sum(quadrants_ts,1),2));%min(min_quadrants);
+        phase_correction(ii,jj) = cand_phase_corrs(which_corr);
+    end
+end
+
+overair_data = overair_data.*exp(1i*repmat(shiftdim(phase_correction,-2),[size(overair_data,1),size(overair_data,2)]));
+
 deconvolved = zeros(size(overair_data,1)/2*size(overair_data,3),size(overair_data,4),length(tag_freq_search_space));
 for tag_freq_offset_idx = 1:length(tag_freq_search_space)
     tag_freq = 1./TAG_FREQ*(1+tag_freq_search_space(tag_freq_offset_idx));
@@ -56,10 +85,12 @@ for tag_freq_offset_idx = 1:length(tag_freq_search_space)
     %mixing_signal = sign(cos(2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times));
     %mixing_signal = exp(1i.*2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times);
     %mixing_signal = cos(2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times);
-    mixing_signal = ones(size(overair_data_times));
-    %mixing_signal = mixing_signal.*repmat(blackman(size(mixing_signal,1)),[1,size(mixing_signal,2),size(mixing_signal,3)]);
+    mixing_signal = sin(2.*pi./repmat(shiftdim(tag_freq,-1),[size(overair_data_times,1),size(overair_data_times,2),1]).*overair_data_times);
+    %mixing_signal = ones(size(overair_data_times));
+    mixing_signal = mixing_signal.*repmat(blackman(size(mixing_signal,1)),[1,size(mixing_signal,2),size(mixing_signal,3)]);
     overair_data_temp = overair_data.*repmat(shiftdim(mixing_signal,-1),[size(overair_data,1),1,1,1]);
     %overair_data_temp = overair_data;
+    blah = squeeze(max(20*log10(abs(fft(overair_data,[],2))),[],1));
     %keyboard;
 
     %Average across time...
@@ -73,6 +104,7 @@ for tag_freq_offset_idx = 1:length(tag_freq_search_space)
     
     %Deconvolution is a simple division...
     deconvolved_fft = overair_data_fft./repmat(cal_data_fft,[1,1,size(overair_data_fft,3)]);
+    %keyboard;
     
     %Center DC...
     deconvolved_fft = fftshift(deconvolved_fft,1);
@@ -91,5 +123,6 @@ for tag_freq_offset_idx = 1:length(tag_freq_search_space)
     
     %Inverse FFT = CIR
     deconvolved(:,:,tag_freq_offset_idx) = ifft(deconvolved_fft,[],1);
+    %keyboard;
 
 end
